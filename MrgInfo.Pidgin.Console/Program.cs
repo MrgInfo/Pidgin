e065b091f028b08d70f5f1988ce15d49ff27df18
Groma István <wavezone@mrginfo.com>
2016-03-19 00:01:16 +0100

    Console shell

---
+++ MrgInfo.Pidgin.Console/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace MrgInfo.Pidgin.Console
+{
+    using Antlr4.Runtime;
+    using Compiler;
+    using static System.Console;
+
+    static class Program
+    {
+        const string PidginSample = 
+@"{ 
+    1 = 2, 
+  2 = {}, 
+  3 }
+";
+
+        static void Main()
+        {
+            var inputStream = new AntlrInputStream( PidginSample.TrimStart() );
+            var lexer = new BasicLexer( inputStream );
+            var commonTokenStream = new CommonTokenStream( lexer );
+            var parser = new BasicParser( commonTokenStream );
+            var context = parser.message();
+            WriteLine( context );
+            ReadKey( true );
+        }
+    }
+}
---
+++ MrgInfo.Pidgin.Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle( "Mrginfo.Pidgin.Console" )]
+[assembly: AssemblyDescription( "Pidgin console tool" )]
+[assembly: AssemblyCompany( "MRG-Infó Bt." )]
+[assembly: AssemblyProduct( "Pidgin" )]
+[assembly: AssemblyTrademark( "Pidgin™" )]
+[assembly: AssemblyCopyright( "Copyright © MRG-Info 2016" )]
+
+[assembly: ComVisible( false ), Guid( "1d54d216-2a86-44fe-8721-67c6959ceb75" )]
+[assembly: CLSCompliant( true )]
+
+[assembly: AssemblyVersion( "1.0.*" )]
+[assembly: AssemblyInformationalVersion( "1.0.0-alpha1" )]
